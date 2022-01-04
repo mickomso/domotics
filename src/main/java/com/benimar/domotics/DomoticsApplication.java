e686d40bb1444b7dda5eed3245487be225c5a318
Miguel Company <miggelvalen@hotmail.com>
2022-01-04 14:56:11 +0100

    First commit

---
+++ src/main/java/com/benimar/domotics/DomoticsApplication.java
@@ -0,0 +1,13 @@
+package com.benimar.domotics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomoticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomoticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benimar/domotics/DomoticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benimar.domotics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomoticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
