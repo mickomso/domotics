a96e4fdc2e7808e7401534ca607c19588cf2d0df
Miguel Company <miggelvalen@hotmail.com>
2022-01-10 21:59:26 +0100

    Reorder code

--- src/main/java/com/benimar/domotics/config/SecurityConfig.java
+++ src/main/java/com/benimar/domotics/config/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+        http.httpBasic();
+        http.authorizeRequests().anyRequest().authenticated();
     }
 }
